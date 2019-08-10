021402a8c16f305875ef3b597edcaea45ff3c210
Brian Schiller <bgschiller@gmail.com>
2019-08-10 10:24:06 -0600

    created empty improved file

---
+++ livecode/src/improved.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser = require('body-parser');
+import cookieParser = require('cookie-parser');
+import {
+  Connection,
+  withConnection,
+  decodeBody,
+  NotAuthorized,
+  json,
+  Forbidden,
+} from 'fascia';
+import {
+  verifyLogin,
+  findUserById,
+  destroyAllTalks,
+  createTalk,
+  User,
+} from './shared';
+import * as t from 'io-ts';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/login', (req, res) => {});
+
+app.post('/talks', (req, res) => {});
+
+app.delete('/talks', (req, res) => {});
+
+export default app;
